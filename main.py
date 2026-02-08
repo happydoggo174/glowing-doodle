360cf5335970955fe2ea6c3e36124cab797e3e59
happydoggo174 <nguyenhaithanh26022009@gmail.com>
2026-02-08 10:50:59 +0700

    add sanity check

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ import uvicorn
 from fastapi.staticfiles import StaticFiles
 app.mount("/user/image",StaticFiles(directory="user/image/"))
 app.mount("/page",StaticFiles(directory="page/"))
+@app.get("/")
+async def ping():
+    return 0
 uvicorn.run(app)
